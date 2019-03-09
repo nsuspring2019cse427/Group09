c884047d331f6fc22a1b08ea512417f7d86f839f
NahiyanNaser <47280237+NahiyanNaser@users.noreply.github.com>
2019-03-10 03:28:02 +0600

    Starting out testinf for each class
    Begining test

---
+++ Project/src/codes/Backgroundtest.java
@@ -0,0 +1,14 @@
+package codes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Backgroundtest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Project/src/codes/Testforlevel.java
@@ -0,0 +1,14 @@
+package codes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Testforlevel {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
