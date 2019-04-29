7d902fd4b372c58e6106ec5490d9429068a0574b
NahiyanNaser <47280237+NahiyanNaser@users.noreply.github.com>
2019-04-30 02:58:08 +0600

    Report Submitted

---
+++ Project01/code/src/codes/Level1Test.java
@@ -0,0 +1,14 @@
+package codes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Level1Test {
+
+	@Test
+	public void testInit() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
